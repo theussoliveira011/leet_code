ea1f1ef1ba76183ec6d9e6ea049ef0d83e120a87
theussoliveira011 <106394398+theussoliveira011@users.noreply.github.com>
2022-05-31 20:41:17 -0300

    Add files via upload

---
+++ exercise_14_leet_code.js
@@ -0,0 +1,10 @@
+///////////////Exercise 14 leet code/////////////////////////////
+var longestComoonPrefix = function(strs) {
+    if(strs.length == 0) return "";
+    for(i = 0; i < strs[0].length; i++) {
+        if(!strs.every((string) => string[i] === strs[0][i])) {
+            return strs[0].slice(0, i);
+        }
+        }
+        return strs[0];
+}
